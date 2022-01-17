726809e505c3da210e1c7d0fa5b6e23558dec95a
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-01-17 18:52:12 -0300

    Adding vuetify, and changing the pagination component

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,25 +1,11 @@
 <template>
-  <div>
-    <nav aria-label=" navigation example">
-      <ul class="container d-flex justify-content-center m-0 p-0">
-        <li>
-          <a @click="Previous">Previous</a>
-        </li>
-
-        <li v-for="(page, index) in TotalPages" :key="index">
-          <a
-            class="link"
-            :class="{ active: currentPage == page }"
-            @click="navigate(page)"
-            >{{ page }}
-          </a>
-        </li>
-
-        <li>
-          <a @click="Next">Next</a>
-        </li>
-      </ul>
-    </nav>
+  <div class="text-center">
+    <v-pagination
+      v-model="page"
+      :length="length"
+      :total-visible="6"
+      circle
+    ></v-pagination>
   </div>
 </template>
 
@@ -29,80 +15,35 @@ export default {
   props: { totalRepositories: Number },
   data() {
     return {
-      TotalPages: [],
-      currentPage: this.$route.query.page || 1,
+      length: [],
+      page:Number,  
+      QueryParams: Number
     };
   },
   methods: {
     Calculating() {
-      this.TotalPages = Math.ceil(this.totalRepositories / 6);
-    },
-    navigate(page) {
-      this.currentPage = page;
-      this.EmitCurrentPage();
-    },
-    Previous() {
-      if (this.currentPage > 1) {
-        this.currentPage--;
-      }
-      this.EmitCurrentPage();
-    },
-    Next() {
-      if (this.currentPage < this.TotalPages) {
-        this.currentPage++;
-      }
-      this.EmitCurrentPage();
+      this.length = Math.ceil(this.totalRepositories / 6);
     },
     SavingRoute() {
-      this.$router.replace({ query: { page: this.currentPage } });
+      this.$router.replace({ query: { page: this.page } });
     },
-    EmitCurrentPage() {
-      this.$emit("navigate", this.currentPage);
+    EmitPage() {
+      this.$emit("navigate", this.page);
     },
+    ConvertTypeRoute(){
+        this.QueryParams =  this.$route.query.page
+        this.page =  Number(this.QueryParams) || 1
+    }
   },
   watch: {
-    currentPage() {
-      this.SavingRoute();
+    page() {
+      this.EmitPage();
+      this.SavingRoute(); 
     },
   },
   created() {
     this.Calculating();
-    this.SavingRoute();
+    this.ConvertTypeRoute();
   },
 };
 </script>
-
-<style scoped>
-nav ul {
-  flex-wrap: wrap;
-}
-nav ul li{
-  height:40px
-}
-nav ul li a {
-  padding: 8px 10px;
-  text-decoration: none;
-}
-nav ul li .active {
-  background-color: #344050;
-  border: 1px solid #161b22;
-
-  color: #fff;
-}
-body.dark nav ul li .active {
-  background-color: #344050;
-}
-nav ul li a {
-  color: #161b22;
-  background-color: #fff;
-
-  border: 1px solid #161b22;
-
-  cursor: pointer;
-}
-body.dark nav ul li a {
-  border: 1px solid #fff;
-  background-color: #161b22;
-  color: #fff;
-}
-</style>
