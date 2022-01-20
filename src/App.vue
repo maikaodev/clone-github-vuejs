2cae966d298e841c89b2ae56a5240c7f0f5591e4
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-01-20 14:09:32 -0300

    Removing CSS style Dark Theme, because now i'm using dark theme by vuetify

--- src/App.vue
+++ src/App.vue
@@ -31,38 +31,4 @@ html,
 body {
   width: 100%;
 }
-/*
-body,
-body a,
-body i,
-body h2,
-body h3,
-body h4,
-body strong {
-  color: #161b22;
-  background-color: #fff;
-}
-
-body.dark,
-body.dark a,
-body.dark i,
-body.dark strong,
-body.dark h2,
-body.dark h3,
-body.dark h4 {
-  background-color: #161b22;
-  color: #fff;
-}
-body.dark .container header .twitter i,
-body.dark .container header .followers i,
-body.dark .container header .followers li span {
-  color: #8b949e;
-}
-body.dark footer button,
-body.dark div.error button {
-  background-color: #fff;
-  color: #161b22;
-  border: 2px solid #fff;
-  font-weight: bold;
-} */
 </style>
