a9e7ce70b0c5c02e0dafec492455eb4457d3ba9f
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-01-20 14:44:42 -0300

    Erasing css to use the default colors of the dark theme

--- src/components/errorNotFound.vue
+++ src/components/errorNotFound.vue
@@ -1,41 +1,29 @@
 <template>
   <div>
     <div
-      class="error container d-flex flex-column justify-content-center align-items-center"
+      class="container d-flex flex-column justify-content-center align-items-center"
     >
       <h1>error, user not found...</h1>
-      <router-link class="text-center" to="/"> Reset system clone </router-link>
+      <v-btn elevation="2" class="primary secondary--text text-center" to="/">
+        Reset system clone
+      </v-btn>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ErrorNotFound'
-}
+  name: "ErrorNotFound",
+};
 </script>
 <style scoped>
-/* END FOOTER */
-.error {
+div {
   height: 80vh;
   margin: 0 auto;
 }
-.error a {
-  width: 200px;
-  padding: 4px 8px;
-
+div a {
   font-weight: bold;
 
-  background-color: #161b22;
-  color: #fff;
-  border: 2px solid #161b22;
   text-decoration: none;
 }
-
-body.dark .error a {
-  background-color: #fff;
-  border: 2px solid #fff;
-  color: #161b22;
-}
-
 </style>
