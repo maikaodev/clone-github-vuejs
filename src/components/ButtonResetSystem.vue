e4152e2a00db7ff7f3670e21989295ed171abe71
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-01-20 18:18:56 -0300

    Creating the component I'm using it in multiple components

---
+++ src/components/ButtonResetSystem.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn elevation="2" class="primary secondary--text text-center" to="/">
+    Reset system clone
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "ButtonResetSystem",
+};
+</script>
