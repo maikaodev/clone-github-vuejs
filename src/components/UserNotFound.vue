eecfb1b46bb75bc0688983dcb30f9c32d36e7e75
Angelo Danrley <82960620+maikaodev@users.noreply.github.com>
2022-01-21 17:02:35 -0300

    Changing the component name

---
+++ src/components/UserNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div
+      class="container d-flex flex-column justify-content-center align-items-center"
+    >
+      <h1>User not found</h1>
+      <ButtonResetSystem />
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonResetSystem from "./ButtonResetSystem.vue";
+export default {
+  name: "UserNotFound",
+  components: { ButtonResetSystem },
+};
+</script>
+<style scoped>
+div {
+  height: 95vh;
+  margin: 0 auto;
+}
+div a {
+  font-weight: bold;
+  text-decoration: none;
+}
+</style>
